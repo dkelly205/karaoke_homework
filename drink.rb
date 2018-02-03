9ba58fe51ce846a94c26473bb7b200f37275df70
daniel kelly <danieljkelly91@gmail.com>
2018-02-03 18:22:29 +0000

    drink class added

---
+++ drink.rb
@@ -0,0 +1,11 @@
+class Drink
+
+  attr_reader :name, :price, :quantity
+
+  def initialize(name, price, quantity)
+    @name = name
+    @price = price
+    @quantity = quantity
+  end
+
+end
---
+++ specs/drink_spec.rb
@@ -0,0 +1,24 @@
+require('minitest/rg')
+require('minitest/autorun')
+require_relative('../drink.rb')
+
+
+
+class DrinkTest < MiniTest::Test
+
+  def setup
+    @drink = Drink.new("beer", 2.99, 1)
+  end
+
+  def test_drink_name
+    assert_equal("beer", @drink.name)
+  end
+
+  def test_drink_price
+    assert_equal(2.99, @drink.price)
+  end
+
+  def test_drink_quantity
+    assert_equal(1, @drink.quantity)
+  end
+end
